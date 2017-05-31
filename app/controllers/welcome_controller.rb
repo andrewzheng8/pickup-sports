da07fc1b4a16ec4bc2ee4674432a886740d069b1
kylewelt <kylewelt@umich.edu>
2017-05-31 16:12:33 -0400

    updated welcome category sorting

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,9 @@
 class WelcomeController < ApplicationController
 
   def index
-    @games = Game.all
-    @sports = Sport.all
+    @upcoming_games = Game.order(:start)
+    @popular_games = Game.popular_games
+    @popular_sports = Sport.popular_sports
   end
 
 end
--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,3 +1,13 @@
 class Sport < ApplicationRecord
-  has_many :games 
+  has_many :games
+
+  def self.popular_sports
+    Sport.
+    select("sports.*, count(games.id)").
+    joins(:games).
+    group("sports.id").
+    order("count(games.id) DESC").
+    limit(10)
+  end
+
 end
