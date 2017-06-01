015c3895182b32a899154e5411212f52dc3c3c98
Alexandra Williams <alexandrawilliams.aiw@gmail.com>
2017-06-01 10:29:25 -0400

    implemented uploader

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,9 @@
 class Player < ApplicationRecord
   has_secure_password
+  attr_accessor :avatar
   has_many :player_games
   has_many :games, through: :player_games
   has_many :posts
   has_many :user_friends
+  mount_uploader :avatar, AvatarUploader
 end
